8e2322f9b806a4fbd6c78f25d7146e80123c7951
sinasun <sinakhodaveisi@gmail.com>
2023-12-25 12:41:12 -0500

    :zap: Improved login logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,23 @@
 import NextAuth from 'next-auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { authOptions } from '@/lib/auth';
 import { NextRequest } from 'next/server';
+
+import type { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { Adapter } from 'next-auth/adapters';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma) as Adapter,
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+		})
+	],
+	session: { strategy: 'jwt' }
+};
 
 function handler(req: NextRequest, res: NextApiResponse) {
 	return NextAuth(req as unknown as NextApiRequest, res, authOptions);
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '@/context/auth-provider';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/lib/auth';
 import { Navbar } from '@/components';
 import NextProvider from '@/context/nextui-provider';
 const inter = Inter({ subsets: ['latin'] });
@@ -18,7 +17,7 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const session = await getServerSession(authOptions);
+	const session = await getServerSession();
 
 	return (
 		<html lang="en" className="light">
