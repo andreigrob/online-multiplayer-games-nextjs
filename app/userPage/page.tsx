96b187042ca4ccf85bbc57792888b8b233111ba4
Andrei Grobnic <andrey.grobnik@gmail.com>
2023-12-25 12:28:34 -0500

    Added user page information

--- app/userPage/page.tsx
+++ app/userPage/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+import { useSession, signIn } from 'next-auth/react';
+
 export default function UserPage() {
+	const { data: session } = useSession();
 	return (
 		<>
 			Signed in <br />
+			{session?.user?.name}
+			<br />
+			{session?.user?.email}
+			<br />
 		</>
 	);
 }
